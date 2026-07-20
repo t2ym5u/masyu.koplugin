f38b028301326c9f90e1698fbe17294c51e0533c
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:19:36 +0200

    i18n: add missing FR translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,3 +1,5 @@
 return {
     ["Circles on path: %1/%2  \xC2\xB7  Path cells: %3"] = { fr = "Cercles sur le tracé : %1/%2  ·  Cases du tracé : %3" },
+    ["Draw a loop through black and white pearls"] = { fr = "Tracez une boucle passant par les perles noires et blanches" },
+    ["Masyu"] = { fr = "Masyu" },
 }

74e4cccd3766c4c9fbb7835eee56ed630939d0b7
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-21 13:40:41 +0200

    chore: bump version to 1.1.3

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "masyu",
     fullname    = _("Masyu"),
     description = _("Draw a loop through black and white pearls"),
-    version     = "1.1.2",
+    version     = "1.1.3",
 }

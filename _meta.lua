9c2d7f890f85f9661240a459efb6305c812ee6e2
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-04 12:50:55 +0200

    chore: bump version to 1.1.12

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Masyu"),
     description = _("Draw a loop through black and white pearls"),
-    version     = "1.1.11",
+    version     = "1.1.12",
 }

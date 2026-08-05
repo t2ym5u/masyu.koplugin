7db3edf3121347e1a5d476fd048539d3381aa9c8
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:59:16 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,5 +1,5 @@
 return {
-    ["Circles on path: %1/%2  \xC2\xB7  Path cells: %3"] = { fr = "Cercles sur le tracé : %1/%2  ·  Cases du tracé : %3" },
-    ["Draw a loop through black and white pearls"] = { fr = "Tracez une boucle passant par les perles noires et blanches" },
-    ["Masyu"] = { fr = "Masyu" },
+    ["Circles on path: %1/%2  \xC2\xB7  Path cells: %3"] = { fr = "Cercles sur le tracé : %1/%2  ·  Cases du tracé : %3", es = "Círculos en el trazado: %1/%2  \xC2\xB7  Casillas del trazado: %3", de = "Kreise auf dem Pfad: %1/%2  \xC2\xB7  Pfadfelder: %3" },
+    ["Draw a loop through black and white pearls"] = { fr = "Tracez une boucle passant par les perles noires et blanches", es = "Traza un bucle que pase por las perlas negras y blancas", de = "Zeichne eine Schleife durch schwarze und weiße Perlen" },
+    ["Masyu"] = { fr = "Masyu", es = "Masyu", de = "Masyu" },
 }
